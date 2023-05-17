a39339f2f98a3f1087e908f791ec07e8c9d44420
Роман Юдешко <romyud1994@gmail.com>
2023-05-17 22:55:36 +0200

    home page added

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:wodobro/presentation/widgets/expandable_fab.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,9 +11,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page', style: Theme.of(context).textTheme.displayLarge),
-      ),
-    );
+        body: Center(
+          child: Text('Home Page',
+              style: Theme.of(context)
+                  .textTheme
+                  .displayMedium
+                  ?.copyWith(color: Colors.black38)),
+        ),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.water),
+            )
+          ],
+        ));
   }
 }
