397a837bbaacaa9c98b1a5860e06b11a74591878
Роман Юдешко <romyud1994@gmail.com>
2023-05-18 12:05:32 +0200

    fab added to homescreen, addsip method added

--- lib/domain/diary_controller.dart
+++ lib/domain/diary_controller.dart
@@ -1,5 +1,12 @@
-class DiaryDomainController{
-  getDiary() async{
-    // return await locator<DiaryRepo>().getDiary();
+import 'package:wodobro/locator.dart';
+import 'package:wodobro/data/diary_repo.dart';
+
+class DiaryDomainController {
+  getDiary() async {
+    return await locator<DiaryRepo>().loadDiary();
+  }
+
+  addSip(String? date, int amount, String? time) async{
+    return await locator<DiaryRepo>().addSip(date == null ? "today" : date, amount, time);
   }
 }
--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:wodobro/presentation/widgets/expandable_fab.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wodobro/domain/diary_controller.dart';
+import 'package:wodobro/locator.dart';
+import 'package:wodobro/presentation/widgets/add_close_animatedicon.dart';
+import 'package:animate_icons/animate_icons.dart';
+import 'package:wodobro/locator.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,11 +24,40 @@ class HomePage extends StatelessWidget {
                   ?.copyWith(color: Colors.black38)),
         ),
         floatingActionButton: SpeedDial(
-          animatedIcon: AnimatedIcons.menu_close,
+          icon: Icons.add,
+          overlayColor: Colors.black,
+          overlayOpacity: 0.3,
           children: [
             SpeedDialChild(
-              child: Icon(Icons.water),
-            )
+              child: Text('100 ml',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium
+                      ?.copyWith(color: Colors.black45)),
+              onTap: () {
+                locator.get<DiaryDomainController>().addSip(null, 100, null);
+              },
+            ),
+            SpeedDialChild(
+                child: Text('200 ml',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: Colors.black45)),
+                onTap: () {
+                  locator.get<DiaryDomainController>().addSip(null, 200, null);
+                }
+            ),
+            SpeedDialChild(
+                child: Text('500 ml',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: Colors.black45)),
+              onTap: () {
+                locator.get<DiaryDomainController>().addSip(null, 500, null);
+              }
+            ),
           ],
         ));
   }
