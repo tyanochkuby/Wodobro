8c2b6e067d4d215a4a9ed6493ce8e7426a0ffa14
Роман Юдешко <romyud1994@gmail.com>
2023-05-30 23:09:03 +0200

    cloud firestore sync added v0.1

--- lib/data/diary_repo.dart
+++ lib/data/diary_repo.dart
@@ -1,6 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:wodobro/application/locator.dart';
 import 'package:wodobro/domain/models/diary_entry.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
@@ -8,22 +12,61 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class DiaryRepo {
   Future<void> saveDiary(List<DiaryEntry> diary) async {
     //if (kIsWeb) return;
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/.wodobro/diary.json');
-    await file.writeAsString(jsonEncode(diary));
+    if(Platform.isAndroid || kIsWeb)
+    {
+      if(FirebaseAuth.instance.currentUser != null)
+        {
+          final List<Map> mapedDiary = []; //= diary.map((e) => e.toJson()).toList();
+          diary.forEach((element) {
+            mapedDiary.add(element.toJson());
+          });
+
+          final user = FirebaseAuth.instance.currentUser!;
+          final db = FirebaseFirestore.instance;
+          final diaryToSave = <String, dynamic>{
+            'user': user.uid,
+            'diary': mapedDiary,
+          };
+          db.collection('diaries').add(diaryToSave).then((DocumentReference doc) =>
+          print('DocumentSnapshot added with ID: ${doc.id}'));
+          return;
+        }
+      if(Platform.isAndroid)
+        locator.get<GetStorage>().write('Diary', diary);
+      return;
+    }
+    if(Platform.isWindows)
+    {
+      final directory = await getApplicationDocumentsDirectory();
+      final file = File('${directory.path}/.wodobro/diary.json');
+      await file.writeAsString(jsonEncode(diary));
+      return;
+    }
   }
 
   Future<List<DiaryEntry>> loadDiary() async {
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/.wodobro/diary.json');
-    if (await file.exists()) {
-      final diary = jsonDecode(await file.readAsString());
-      return diary
-          .map<DiaryEntry>((json) => DiaryEntry.fromJson(json))
-          .toList();
-    } else {
-      return [];
+    if(Platform.isAndroid)
+      {
+        final diary = locator.get<GetStorage>().read('Diary');
+        if(diary == null)
+          return [];
+        else
+          return diary;
+      }
+    if(Platform.isWindows) {
+      final directory = await getApplicationDocumentsDirectory();
+      final file = File('${directory.path}/.wodobro/diary.json');
+      if (await file.exists()) {
+        final diary = jsonDecode(await file.readAsString());
+        return diary
+            .map<DiaryEntry>((json) => DiaryEntry.fromJson(json))
+            .toList();
+      } else {
+        return [];
+      }
     }
+    else
+      return [];
   }
 
   Future<void> addSip(String date, int amount, String? time) async {
---
+++ lib/presentation/tips_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TipsPage extends StatelessWidget {
+  const TipsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Tips Page')),);
+  }
+}
