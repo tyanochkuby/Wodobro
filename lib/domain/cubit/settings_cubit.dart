4a854860811adf6e88615412e889d4d0ab0fb347
tyanochku_by <ramyud@st.amu.edu.pl>
2023-08-30 14:12:49 +0200

    hydrated mixin added to settings cubit

--- lib/domain/cubit/settings_cubit.dart
+++ lib/domain/cubit/settings_cubit.dart
@@ -2,11 +2,22 @@ import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
 import 'dart:convert';
 
 part 'settings_state.dart';
 
-class SettingsCubit extends Cubit<SettingsState> {
-  SettingsCubit() : super(SettingsInitial());
+class SettingsCubit extends Cubit<SettingsState> with HydratedMixin {
+  SettingsCubit() : super(SettingsState(notificationsEnabled: false));
+
+  @override
+  SettingsState? fromJson(Map<String, dynamic> json) {
+    return SettingsState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(SettingsState state) {
+    return state.toMap();
+  }
 }
